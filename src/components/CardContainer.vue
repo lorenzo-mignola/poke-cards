42a50c5af9cd8325508a9cb15b596a8ef1792729
lorenzo-mignola <lo.mignola@gmail.com>
2022-03-11 18:31:58 +0100

    Add card component

---
+++ src/components/CardContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="glass card">
+    <h1 class="text-center text-6xl bold pt-3 uppercase">Lorenzo card</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.card {
+  width: 80vw;
+  height: 80vh;
+}
+</style>
