e6140c3554bcddcd383862d8fe196f3e5b66945a
lorenzo-mignola <lo.mignola@gmail.com>
2022-03-12 10:39:20 +0100

    Create card container

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="glass card">
-    <h1 class="text-center text-6xl bold pt-3 uppercase">Lorenzo card</h1>
+  <div class="glass-card card">
+    <h1
+      class="glass text-6xl bold capitalize shadow-none p-6 flex items-center w-full justify-center"
+    >
+      <PokeBall class="pokeball mr-3" />
+      Lorenzo card
+      <PokeBall class="pokeball ml-3" />
+    </h1>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import PokeBall from "../assets/Poké_Ball_icon.svg";
+</script>
 
 <style lang="scss" scoped>
 .card {
   width: 80vw;
   height: 80vh;
+}
+
+h1 {
+  font-family: "Poppins", sans-serif;
+}
+
+.pokeball {
+  width: 2.5rem;
+  height: 2.5rem;
 }
 </style>
